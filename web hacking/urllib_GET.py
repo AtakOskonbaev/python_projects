72add3d84a4c26c2c1ff041dfc526f698db923f9
AtakOskonbaev <atakoskonbaev21@gmail.com>
2025-05-16 13:47:49 +0600

    added user agent header

--- web hacking/urllib_GET.py
+++ web hacking/urllib_GET.py
@@ -1,5 +1,9 @@
 import urllib.request
 url = 'https://dcb.kg/'
+
+headers = {'User-Agent':'GoogleBot'}
+request = urllib.request.Request(url, headers=headers)
+
 response = urllib.request.urlopen(url)
 print(response.read())
 response.close()
